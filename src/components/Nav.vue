8b2f0f87fde0217f911c1dcb41d79db1d005978a
JavierMorales0 <javiermoralesmelara@gmail.com>
2022-02-16 19:22:37 -0600

    Iniciada la segunnda navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,193 +1,42 @@
 <template>
   <nav class="_nav">
-    <div class="_nav-brand h-100 _w-25">
-      <img :src="require('@/assets/illustrations/options.svg')" alt="Logo" />
-      <span>Texto extra</span>
+    <div>a</div>
+    <div class="_w-25">
+      <el-input class="w-100" v-model="busqueda">
+        <template #prefix>
+          <el-icon class="el-input__icon"><search /></el-icon>
+        </template>
+      </el-input>
     </div>
-    <div
-      class="_nav-options"
-      :class="{
-        '_nav-options-show': mostrarElementosMenu,
-      }"
-    >
-      <ul>
-        <router-link
-          class="_nav-item"
-          v-for="(item, index) in rutas"
-          :key="index"
-          :to="item.direccion"
-        >
-          {{ item.nombre }}
-          <ul class="position-absolute d-none">
-            <li class="position-absolute top-0 right0">asdasdasd</li>
-          </ul>
-        </router-link>
-      </ul>
-    </div>
-    <div class="_nav-burger">
-      <img
-        :src="require('@/assets/icons/menus.png')"
-        alt="burger-menu-icon"
-        class="nav-burger"
-        ref="burger"
-        v-on:click="cambiarEstadoElementosMenu"
-      />
-    </div>
+    <div>C</div>
   </nav>
 </template>
 <script>
+import { Search } from "@element-plus/icons-vue";
 export default {
+  components: {
+    Search,
+  },
   data() {
     return {
-      mostrarElementosMenu: true,
-      rutas: [
-        {
-          direccion: "/",
-          nombre: "Inicio",
-          poseeDerivaciones: false,
-        },
-        {
-          direccion: "",
-          nombre: "Inventario",
-          poseeDerivaciones: true,
-          derivaciones: [
-            {
-              direccion: "/inventario/productos",
-              nombre: "Productos",
-            },
-            {
-              direccion: "/inventario/insumos",
-              nombre: "Insumos",
-            },
-          ],
-        },
-        {
-          direccion: "",
-          nombre: "Movimientos",
-          poseeDerivaciones: true,
-          derivaciones: [
-            {
-              direccion: "/movimientos/ingresos",
-              nombre: "Ingresos",
-            },
-            {
-              direccion: "/movimientos/egresos",
-              nombre: "Egresos",
-            },
-            {
-              direccion: "/movimientos/ventas",
-              nombre: "Ventas",
-            },
-            {
-              direccion: "/movimientos/extra",
-              nombre: "Extras",
-            },
-          ],
-        },
-        {
-          direccion: "/producciones",
-          nombre: "Producciones",
-          poseeDerivaciones: false,
-        },
-        {
-          direccion: "/perfil",
-          nombre: "Perfil",
-          poseeDerivaciones: false,
-        },
-      ],
+      busqueda: "",
     };
-  },
-  methods: {
-    cambiarEstadoElementosMenu() {
-      this.mostrarElementosMenu = !this.mostrarElementosMenu;
-    },
   },
 };
 </script>
 <style lang="scss">
 ._nav {
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 0.5rem 1rem;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-  min-height: 7vh;
-}
-._nav-brand {
-  display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: row;
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: #000;
-  height: 100%;
-  margin: 12px 0px;
-  background: rebeccapurple;
+  height: 10vh;
+  padding: 0 2rem;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
 }
-._nav-options {
-  display: none;
-  text-align: start;
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  height: 100%;
-  ul {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    padding: 0;
-    margin: 0;
-    ._nav-item {
-      display: block;
-      text-align: start;
-      list-style: none;
-      padding: 0.5rem 0;
-      font-size: 1rem;
-      transition: 0.5s ease;
-      text-decoration: none;
-      color: var(--gray);
-      &:hover {
-        color: var(--color-primary) !important;
-      }
-    }
-  }
-}
-._nav-options-show {
-  display: flex;
-}
-.nav-burger {
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-  position: absolute;
-  top: 15px;
-  right: 10px;
-  visibility: visible;
-}
+
 @media (min-width: 768px) {
   ._nav {
-    flex-direction: row;
-    min-height: 10vh;
-  }
-  .nav-burger {
-    visibility: hidden !important;
-  }
-  ._nav-options {
-    visibility: visible;
-    min-width: 50%;
-    ul {
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      ._nav-item {
-        margin: 0;
-        padding: 5px 8px !important;
-        font-size: 1rem;
-      }
-    }
   }
 }
 </style>
